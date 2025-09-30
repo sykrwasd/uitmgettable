b7360fae4b71200aab7fea0ed64464cb990d506e
sykrwasd <umarsyakir16@gmail.com>
2025-09-30 15:59:43 +0800

    add error message if subject doesnt exist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script"; // ✅ import Script
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
-  weight: ['400', '500', '600'], // pick the weights you need
-  subsets: ['latin'],             // important
-  variable: '--font-poppins',     // optional CSS variable
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
 });
+
 export const metadata: Metadata = {
   title: "UitmGetTable",
   description: "UiTM Timetable Generator",
   icons: {
     icon: "/img/logo.png",
+    shortcut: "/img/logo.png",
+    apple: "/img/logo.png",
   },
 };
-
 
 export default function RootLayout({
   children,
@@ -24,11 +26,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} antialiased`}>
+        {/* ✅ Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0DR3DM0VE2"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0DR3DM0VE2');
+          `}
+        </Script>
+
         {children}
-        <Analytics/>
       </body>
     </html>
   );
--- src/components/orderError.tsx
+++ src/components/orderError.tsx
@@ -1,4 +1,4 @@
-"use client";
+ "use client";
 import { useState } from "react";
 
 export default function OrderErrorPopup({ message = "Something went wrong!" }) {
